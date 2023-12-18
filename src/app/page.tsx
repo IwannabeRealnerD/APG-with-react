af932893eea91f63d9d64872bdf69a947ef81300
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2023-12-18 21:15:09 +0900

    [feat] improved tap order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import * as style from "./styles.css";
 
 export default function Home() {
+  console.log(
+    "This is Server Component. This console log will appear in server console as well."
+  );
+
   return (
     <main className={style.container}>
       <h2>Main page</h2>
--- src/app/tab/page.tsx
+++ src/app/tab/page.tsx
@@ -5,22 +5,24 @@ import * as style from "./sytyle.css";
 
 const tabInfoArr = [
   {
-    genre: "투어러",
-    desc: "투어러 장르는 bmw motorrad의 긴 역사에서 빼놓을 수 없는 장르입니다.",
+    genre: "Tourer",
+    desc: "Tourer genre is an indispensable genre in the long history of bmw motorrad.",
   },
   {
-    genre: "슈퍼 스포츠",
-    desc: "bmw motorrad 슈퍼 스포츠의 자랑스러운 모델 bmw s1000rr은 새로운 역사를 써내려가는 슈퍼 스포츠 모터사이클 입니다.",
+    genre: "Super Sports",
+    desc: "The proud model of bmw s1000rr, the Super Sports motorcycle, is writing a new history in the Super Sports.",
   },
   {
-    genre: "어드벤쳐",
-    desc: "BMW Motorrad를 대표하는 어드벤쳐 장르입니다. 타브랜드와 비교할수 없는 최고의 경험을 선사합니다.",
+    genre: "adventurer",
+    desc: "It is the adventurer genre that represents BMW Motorrad. It offers an incomparable best experience compared to other brands.",
   },
-];
+] as const;
+
+type GenreType = (typeof tabInfoArr)[number]["genre"];
 
 const TabPage = () => {
-  const [activeMenu, setActiveMenu] = useState("전체");
-  const [focusIndex, setFocusIndex] = useState<number | null>(0);
+  const [activeMenu, setActiveMenu] = useState<GenreType>("Tourer");
+  const [focusIndex, setFocusIndex] = useState<number | null>(null);
   const tabRefList: React.MutableRefObject<
     Map<number, HTMLButtonElement>
   > = useRef(new Map());
@@ -53,56 +55,58 @@ const TabPage = () => {
     };
   }, [focusIndex]);
 
-return (
-  <main className={style.container}>
-    <h2>BMW Motorrad</h2>
-    <div role="tablist">
-      {tabInfoArr.map(({ genre }, index) => {
+  return (
+    <main className={style.container}>
+      <h2>BMW Motorrad</h2>
+      <div role="tablist">
+        {tabInfoArr.map(({ genre }, index) => {
+          const isSelected = genre === activeMenu;
+          return (
+            <button
+              key="tab"
+              type="button"
+              tabIndex={isSelected ? 0 : -1}
+              aria-selected={isSelected}
+              id={`tab-${index + 1}`}
+              role="tab"
+              aria-controls={`tabpanel-${index + 1}`}
+              onFocus={() => setFocusIndex(index)}
+              onBlur={() => setFocusIndex(null)}
+              ref={(domNode) => {
+                if (domNode) {
+                  tabRefList.current.set(index, domNode);
+                  return;
+                }
+                tabRefList.current.delete(index);
+              }}
+              onClick={() => {
+                setActiveMenu(genre);
+              }}
+            >
+              {genre}
+            </button>
+          );
+        })}
+      </div>
+      {tabInfoArr.map(({ genre, desc }, index) => {
         const isSelected = genre === activeMenu;
+        const key = `tabpanel-${index + 1}`;
         return (
-          <button
-            key="tab"
-            type="button"
-            tabIndex={isSelected ? 0 : -1}
-            aria-selected={isSelected}
-            id={`tab-${index + 1}`}
-            role="tab"
-            aria-controls={`tabpanel-${index + 1}`}
-            ref={(domNode) => {
-              if (domNode) {
-                tabRefList.current.set(index, domNode);
-                return;
-              }
-              tabRefList.current.delete(index);
-            }}
-            onClick={() => {
-              setActiveMenu(genre);
-            }}
+          <div
+            key={key}
+            id={key}
+            tabIndex={0}
+            role="tabpanel"
+            aria-labelledby={`tab-${index + 1} ${key}`}
+            aria-label="에 대한 설명"
+            hidden={!isSelected}
           >
-            {genre}
-          </button>
+            <p>{desc}</p>
+          </div>
         );
       })}
-    </div>
-    {tabInfoArr.map(({ genre, desc }, index) => {
-      const isSelected = genre === activeMenu;
-      const key = `tabpanel-${index + 1}`;
-      return (
-        <div
-          key={key}
-          id={key}
-          tabIndex={0}
-          role="tabpanel"
-          aria-labelledby={`tab-${index + 1} ${key}`}
-          aria-label="에 대한 설명"
-          hidden={!isSelected}
-        >
-          <p>{desc}</p>
-        </div>
-      );
-    })}
-  </main>
-);
+    </main>
+  );
 };
 
 export default TabPage;
